502aa75522674d72855306752b6dda6b059a3fa7
Amalya Megerman <amalyamegerman@gmail.com>
2023-03-03 17:50:20 -0500

    add useEffect colorInterval

--- src/Cat.js
+++ src/Cat.js
@@ -1,44 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-import { useHistory } from 'react-router-dom';
-import { colors, codes } from './data';
+import { useHistory } from "react-router-dom";
+import { colors, codes } from "./data";
 
 const Cat = () => {
   const history = useHistory();
   const [colorNum, setColorNum] = useState(0);
-  const [statusChange, setStatusChange] = useState('418');
-  const [status, setStatus] = useState('');
+  const [statusChange, setStatusChange] = useState("418");
+  const [status, setStatus] = useState("");
 
-  const handleSubmit = e => {
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      setColorNum((prevNum) => ++prevNum % colors.length);
+    }, 5000);
+    return () => clearInterval(colorInterval);
+  }, []);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     setStatusChange(status);
-    setStatus('');
+    setStatus("");
   };
 
   return (
     <div
-      className='cat-container'
+      className="cat-container"
       style={{
         backgroundColor: colors[colorNum],
-        transition: 'background-color 4s',
+        transition: "background-color 4s",
       }}
     >
       <h1>Cat Status</h1>
-      <button onClick={() => history.push('/')}>Home</button>
-      <div className='image-container'>
-        <img src={`https://http.cat/${statusChange}`} alt='404' />
+      <button onClick={() => history.push("/")}>Home</button>
+      <div className="image-container">
+        <img src={`https://http.cat/${statusChange}`} alt="404" />
       </div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='cStatus'>
+        <label htmlFor="cStatus">
           <input
-            type='number'
-            id='cStatus'
-            onChange={e => setStatus(e.target.value)}
-            placeholder='find new status'
+            type="number"
+            id="cStatus"
+            onChange={(e) => setStatus(e.target.value)}
+            placeholder="find new status"
             value={status}
           />
         </label>
-        <button type='submit'>Change Status</button>
+        <button type="submit">Change Status</button>
       </form>
     </div>
   );
